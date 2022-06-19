c8350d14d55a22e95b5b35f16b191f3bc4f99e9f
unknown <estudotsi@gmail.com>
2022-06-19 10:39:55 -0300

    foi até template

---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CursosComponent } from './cursos.component';
+import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
+
+
+
+@NgModule({
+  declarations: [
+    CursosComponent,
+    CursosDetalheComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    CursosComponent,
+    CursosDetalheComponent
+  ]
+})
+export class CursosModule { }
